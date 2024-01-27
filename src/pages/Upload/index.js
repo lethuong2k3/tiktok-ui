f7d3e47f4586ace55888c9c87ed0bf0a26241320
LE_ANH_THUONG\thuon <leanhthuong2k3@gmail.com>
2024-01-27 14:35:33 +0700

    update

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,13 @@
+import Header from '~/components/Layout/DefaultLayout/Header';
+
+function Upload() {
+    return (
+        <>
+            <Header />
+            <h2>Upload</h2>
+        </>
+        
+    );
+}
+
+export default Upload;
